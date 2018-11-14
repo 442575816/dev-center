3e351ff69385bede082b3833e07625efa2b9654c
2801659445 <2801659445@qq.com>
2018-11-14 12:04:36 +0800

    svn权限申请提交

--- src/views/svn/SvnAuth.vue
+++ src/views/svn/SvnAuth.vue
@@ -1,21 +1,24 @@
 <template>
-    <el-form>
-        <el-row>
-            <el-col :span="6" style="background:#f9fafc">
-                <el-form-item label="任务名称：">
-                </el-form-item>
-            </el-col>
-            <el-col :span="6" style="background:#f9fafc">
-                <el-form-item label="SVN权限申请">
-                </el-form-item>
-            </el-col>
-        </el-row>
-    </el-form>
+    <div id="body" >
+        <iframe id="workflowIf" :src="iframeURL" frameborder="0" scrolling="no"></iframe>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+      return {
+        iframeURL: "http://127.0.0.1:8011/root/createFlow.action?flowId=SvnAuth_1"
+      }
+    },
+    mounted() {
+        this.iframeURL =  "http://127.0.0.1:8011/root/createFlow.action?flowId=SvnAuth_1";
+        let iframe = document.querySelector('#workflowIf');
+        const deviceWidth = document.documentElement.clientWidth;
+        const deviceHeight = document.documentElement.clientHeight;
+        iframe.style.width = (Number(deviceWidth) - 270) + 'px'; //数字是页面布局宽度差值
+        iframe.style.height = (Number(deviceHeight) + 70) + 'px'; //数字是页面布局高度差
+    }
 }
 </script>
 
